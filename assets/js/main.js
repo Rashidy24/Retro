33dfd6ee9b87422ebb3710d2f7a5d81304d53fa8
Mostafa El-Rashidy <165313756+Rashidy24@users.noreply.github.com>
2024-08-09 02:02:25 +0300

    first upload 1 Page !

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let menu = document.getElementById('menu');
+let box = document.getElementById('box');
+let xMark = document.getElementById('call');
+
+menu.onclick = () => {
+    box.style.display = 'block';
+}
+call.onclick = function() {
+    box.style.display = 'none';
+}
+
